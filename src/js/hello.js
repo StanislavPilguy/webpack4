96093537fb69e15e0a00faffc5191fa920d18352
Sanislav <stanislav.pilguy@gmail.com>
2018-11-24 02:00:42 +0200

    git repository

---
+++ src/js/hello.js
@@ -0,0 +1,4 @@
+export default function bar() {
+	alert('А этого вы точно не ожидали))');
+	alert('Я же говорил? ');
+}
---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+import '../scss/main.scss';
+import 'babel-polyfill';
+import 'bootstrap';
+
+import hello from './hello';
+
+hello();
+
